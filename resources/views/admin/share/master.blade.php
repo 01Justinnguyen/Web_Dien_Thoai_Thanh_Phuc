d6553ad77fe2730c6ab31893486dd1d504f55a59
NguyenPhuc <phuc01112002@gmail.com>
2021-12-01 22:49:19 +0700

    Web ban dien thoai

---
+++ resources/views/admin/share/master.blade.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
+<!-- BEGIN: Head-->
+
+@include('admin.share.headCSS')
+<!-- END: Head-->
+
+<!-- BEGIN: Body-->
+
+<body class="vertical-layout vertical-menu-modern  navbar-floating footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="">
+
+    <!-- BEGIN: Header-->
+    @include('admin.share.header')
+    <!-- END: Header-->
+
+
+    <!-- BEGIN: Main Menu-->
+    @include('admin.share.leftMenu')
+    <!-- END: Main Menu-->
+
+    <!-- BEGIN: Content-->
+    <div class="app-content content ">
+        @yield('content')
+    </div>
+    <!-- END: Content-->
+
+    <div class="sidenav-overlay"></div>
+    <div class="drag-target"></div>
+
+    @include('admin.share.footer')
+    @include('admin.share.footCSS')
+    @yield('js')
+</body>
+<!-- END: Body-->
+
+</html>
